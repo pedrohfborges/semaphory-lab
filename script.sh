28a54ea180e37c995f7956a680ba95521f525a46
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2025-11-26 10:41:06 -0300

    Novo script

--- script.sh
+++ script.sh
@@ -12,17 +12,8 @@ sleep 5
 
 sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "$5"
 
+sudo pkill -9 $6
 
-pid=$(ps -aux | grep nginx | cut -d " " -f5)
-
-
-for i in $(cat $pid)
-
-do 
-
-    kill $i
-    echo "Matamos o PID $i"
-done
 
 
 
