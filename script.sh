84c169da008ef09de9b31cbd2e5bf3ceca66865a
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2025-11-26 10:26:48 -0300

    Novo script

--- script.sh
+++ script.sh
@@ -4,12 +4,14 @@
 #$2=usuario
 #$3=host
 #$4=porta
+#$5=Comando a ser executado
+#Entre aspas o comando desejado
 
-sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "service nginx stop"
+sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "$5"
 
 sleep 5
 
-sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "service nginx start"
+sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "$5"
 
 
 
