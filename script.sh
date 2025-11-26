c52fd7e98954000791def68511ae82fe0ff5d65a
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2025-11-26 10:37:01 -0300

    Novo script

--- script.sh
+++ script.sh
@@ -4,15 +4,25 @@
 #$2=usuario
 #$3=host
 #$4=porta
-#$5=Comando a ser executado
 #Entre aspas o comando desejado
 
-sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "$5"
+sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "service nginx stop"
 
 sleep 5
 
 sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "$5"
 
+
+pid=$(ps -aux | grep nginx | cut -d " " -f5)
+
+
+for i in $(cat $pid)
+
+do 
+
+    kill $i
+    echo "Matamos o PID $i"
+done
 
 
 
