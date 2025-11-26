310c3643c13b9b6e024f414cd43d7ab54032d037
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2025-11-26 11:20:38 -0300

    Novo script

--- script.sh
+++ script.sh
@@ -16,13 +16,15 @@ sleep 2
 
 sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "$5" >> /dev/null
 
-echo "Matando os Processos Fantasmas"
+sleep 3
+
+pkill -9 $6
+
+echo "Matando os Processos Fantasmas do $6"
 
 echo "---------------------------------------------------------------"
 
-sleep 10
-
-pkill -9 $6
+sleep 3 
 
 echo "Iniciado o Serviço do NGINX..."
 
