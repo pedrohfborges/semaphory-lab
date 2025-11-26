c594768770684e32e64b7b043f5738f69432e16d
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2025-11-26 09:49:56 -0300

    Novo script

--- script.sh
+++ script.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p 2200 "service nginx stop"
+#$1=senhassh
+#$2=usuario
+#$3=host
+#$4=porta
+
+sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "service nginx stop"
 
 sleep 5
 
-sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p 2200 "service nginx start"
+sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "service nginx start"
 
 
 
