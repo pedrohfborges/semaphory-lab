042e5790a611ec933be2037ebbbd3d49823be1f7
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2025-11-26 11:09:20 -0300

    Novo script

--- script.sh
+++ script.sh
@@ -6,7 +6,29 @@
 #$4=porta
 #Entre aspas o comando desejado
 
+clear 
+
+echo "Parando o Serviço do NGINX..."
+
+echo "---------------------------------------------------------------"
+
+sleep 2
+
 sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "$5"
+
+echo "Matando os Processos Fantasmas"
+
+echo "---------------------------------------------------------------"
+
+sleep 2
+
+pkill -9 $6
+
+echo "Iniciado o Serviço do NGINX..."
+
+sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p $4 "$7"
+
+
 
 
 
