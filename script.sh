062f44e840297d7695e5c0ad5d6db5ed8389df3e
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2025-11-26 09:15:07 -0300

    Novo script

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-ping $1
+sudo sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p 2200 "service nginx stop"
+
+sleep 5
+
+sudo sshpass -p "$1" ssh -o StrictHostKeyChecking=no $2@$3 -p 2200 "service nginx start"
+
+
+
+
+
+
